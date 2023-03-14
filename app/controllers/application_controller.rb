993f3de278b5e38177ea86d142cfaf6956de84f2
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-03-14 13:34:14 +0000

    [Finish]4-8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     about_path
   end
-  
+
   def after_sign_out_path_for(resource)
     about_path
   end
-  
+
   protected
 
   def configure_permitted_parameters
