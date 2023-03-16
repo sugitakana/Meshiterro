63e924d4a5c1492985c003a16da92230875cfeac
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-03-16 12:07:27 +0000

    [Finish]4-20

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "homes#top"
-   get 'homes/about' => 'homes#about', as:'about'
+  get 'homes/about' => 'homes#about', as:'about'
   
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
      resource :favorites, only: [:create, :destroy]
